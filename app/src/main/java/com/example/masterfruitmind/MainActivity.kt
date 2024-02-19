1ce3e5bce8cde90bc9deb9718e1a8906820db8c5
maelog <mael.dabard@gmail.com>
2024-02-19 17:18:30 +0100

    delete useless import

--- app/src/main/java/com/example/masterfruitmind/MainActivity.kt
+++ app/src/main/java/com/example/masterfruitmind/MainActivity.kt
@@ -3,19 +3,9 @@ package com.example.masterfruitmind
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.masterfruitmind.ui.theme.MasterFruitMindTheme
 import com.google.gson.Gson
 
 class MainActivity : ComponentActivity() {
