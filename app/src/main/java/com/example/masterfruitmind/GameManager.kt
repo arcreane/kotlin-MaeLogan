d037857fc533fb5819c66aa0a602b69d1c5e891e
maelog <mael.dabard@gmail.com>
2024-02-19 16:23:19 +0100

    bug from MakeGuess, the list historyguess are not being update

--- app/src/main/java/com/example/masterfruitmind/GameManager.kt
+++ app/src/main/java/com/example/masterfruitmind/GameManager.kt
@@ -1,11 +1,12 @@
 package com.example.masterfruitmind
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 
 class GameManager(val ListFruit: List<fruits>) {
 
-    val historyListFruit = MutableLiveData<List<List<fruits>>>()
-    val historyGuessFruit = MutableLiveData<List<List<Int>>>()
+    val historyListFruit = MutableLiveData<List<List<fruits>>>(emptyList())
+    val historyGuessFruit = MutableLiveData<List<List<Int>>>(emptyList())
     var shuffleFruits = listOf<fruits>()
     var life = MutableLiveData<Int>()
 
@@ -44,16 +45,17 @@ class GameManager(val ListFruit: List<fruits>) {
 
     fun makeGuess(listFruitsFromUser: List<fruits>) : Boolean{
         val guess = GuessList(listFruitsFromUser);
-
         if(guess.all{it == 1}) {
             return true
         }
         historyListFruit.value = historyListFruit.value?.plus(listOf(listFruitsFromUser))
+
         historyGuessFruit.value = historyGuessFruit.value?.plus(listOf(guess))
         life.value = life.value?.minus(1)
+        println(life.value)
         if(life.value!! <= 0){
-
         }
+        println(historyListFruit.value)
         return false
     }
 
